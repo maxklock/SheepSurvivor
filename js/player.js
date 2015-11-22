10b64e8a69ddf8678a616213925bd6d2266431f5
Maximilian Klockmann <maximilian.web@gmx.net>
2015-11-22 11:20:33 +0100

    sound test

--- js/player.js
+++ js/player.js
@@ -53,7 +53,7 @@ function UpdatePlayer(player) {
 	
 	if (player.position.y < 0.6) {
 		player.position.y = 0.6;
-		if (!sounds.water.isPlaying) {
+		if (!sounds.water.isPlaying || true) {
 			sounds.water.play(1);
 		}
 	} else if (player.position.y >= 0.8 || isSleeping) {
